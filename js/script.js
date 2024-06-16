b58268dd0bbcd2eef9fb78b4d90c2563929abc27
Perun0 <uroda.mar@gmail.com>
2024-06-16 12:18:15 +0200

    Composition and theme switcher updated

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 function changeTheme() {
+
     let html = document.documentElement;
     let button = document.getElementById("themeChanger");
-
     let theme = html.getAttribute("data-theme");
-
-    //TODO: Use switch and make it smarter. It should remember value, when you reload.
+    //TODO: Use switch and make it smarter.
 
     if (theme == "dark") {
+        localStorage.setItem("theme", "light");
         html.setAttribute("data-theme", "light");
         button.setAttribute("class", "icon-sun");
     } else if (theme == "light") {
+        localStorage.setItem("theme", "dark");
         html.setAttribute("data-theme", "dark");
         button.setAttribute("class", "icon-moon");
     }
 }
+
+
+function catchMyTheme() {
+    let html = document.documentElement;
+    let button = document.getElementById("themeChanger");
+    let theme = localStorage.getItem("theme");
+
+    if (theme == "light") {
+        html.setAttribute("data-theme", "light");
+        button.setAttribute("class", "icon-sun");
+
+    } else if (theme == "dark") {
+        html.setAttribute("data-theme", "dark");
+        button.setAttribute("class", "icon-moon");
+
+    }
+}
+
+catchMyTheme();
